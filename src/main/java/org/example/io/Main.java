4fa5fb8b9014d7dcb08021f8b715479713f9bd91
Pavel Shebeko <shebeko_pavel@mail.ru>
2023-02-20 17:54:59 +0300

    add IO block

---
+++ src/main/java/org/example/io/Main.java
@@ -0,0 +1,13 @@
+package org.example.io;
+
+/** IO java предназначено для чтения и записи:
+ * 1. из/в файл Files
+ * 2. из/в сеть Network
+ * 3. из/в память Memory
+ * 4. из/в порты Ports - COM, USB
+ *
+ * Любой ввод/вывод - это поток байтов
+ */
+
+public class Main {
+}
